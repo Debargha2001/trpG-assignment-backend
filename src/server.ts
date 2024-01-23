c427b03bcaaf192ab6daf51a035562d5a08e19ac
Debargha Dutta <debarghadutta2001@gmail.com>
2024-01-23 13:22:00 +0530

    cors error resolved

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import routes from "./routes/routes.v1";
 import morgan from "morgan";
-
-
+import cors from "cors";
 
 const app: Express = express();
 
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(cors());
+app.use(morgan("dev"));
 app.use("/v1", routes);
 
 const port = 8080;
-
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
